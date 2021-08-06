ebe54e7fb2cbde10654371fc0807d4bb2b1e2a21
Thomas Rayner <thmsrynr@outlook.com>
2021-08-06 13:14:02 -0700

    jiggle POC

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using WindowsInput;
+using System.Threading;
+
+namespace mousejiggle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new();
+            InputSimulator inpSim = new();
+
+            while (true)
+            {
+                int x = r.Next(-2, 2);
+                int y = r.Next(-2, 2);
+                Console.WriteLine($"Move x: {x}, y: {y}");
+                inpSim.Mouse.MoveMouseBy(x, y);
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
